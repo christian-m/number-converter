c2a0f6f532c9ca28d8d4154dd0613fbb88880637
Christian Matzat <christian@matzat.dev>
2024-12-13 10:37:49 +0100

    add special cases to roman number conversion

--- src/test/java/dev/matzat/numberconverter/converter/DecimalToRomanConverterTest.java
+++ src/test/java/dev/matzat/numberconverter/converter/DecimalToRomanConverterTest.java
@@ -26,15 +26,46 @@ public class DecimalToRomanConverterTest {
 
     private static Stream<Arguments> intToRomanTestdata() {
         return Stream.of(
-                Arguments.of(0, ""),
-                Arguments.of(1, "I"),
-                Arguments.of(5, "V"),
-                Arguments.of(10, "X"),
-                Arguments.of(50, "L"),
-                Arguments.of(100, "C"),
-                Arguments.of(500, "D"),
-                Arguments.of(1000, "M"),
-                Arguments.of(3000, "MMM")
+            Arguments.of(0, ""),
+            Arguments.of(1, "I"),
+            Arguments.of(2, "II"),
+            Arguments.of(4, "IV"),
+            Arguments.of(5, "V"),
+            Arguments.of(6, "VI"),
+            Arguments.of(9, "IX"),
+            Arguments.of(10, "X"),
+            Arguments.of(11, "XI"),
+            Arguments.of(40, "XL"),
+            Arguments.of(49, "XLIX"),
+            Arguments.of(50, "L"),
+            Arguments.of(51, "LI"),
+            Arguments.of(60, "LX"),
+            Arguments.of(61, "LXI"),
+            Arguments.of(90, "XC"),
+            Arguments.of(99, "XCIX"),
+            Arguments.of(100, "C"),
+            Arguments.of(101, "CI"),
+            Arguments.of(111, "CXI"),
+            Arguments.of(150, "CL"),
+            Arguments.of(151, "CLI"),
+            Arguments.of(400, "CD"),
+            Arguments.of(499, "CDXCIX"),
+            Arguments.of(500, "D"),
+            Arguments.of(501, "DI"),
+            Arguments.of(510, "DX"),
+            Arguments.of(511, "DXI"),
+            Arguments.of(600, "DC"),
+            Arguments.of(900, "CM"),
+            Arguments.of(990, "CMXC"),
+            Arguments.of(999, "CMXCIX"),
+            Arguments.of(1000, "M"),
+            Arguments.of(1001, "MI"),
+            Arguments.of(1100, "MC"),
+            Arguments.of(1101, "MCI"),
+            Arguments.of(1104, "MCIV"),
+            Arguments.of(1110, "MCX"),
+            Arguments.of(3000, "MMM"),
+            Arguments.of(3999, "MMMCMXCIX")
         );
     }
 
@@ -51,6 +82,6 @@ public class DecimalToRomanConverterTest {
     @DisplayName("WHEN an integer out of range is submitted to the converter THEN an IllegalArgumentException is thrown")
     public void testIntToRomanValueOutOfRange(int input) {
         assertThatThrownBy(() -> converter.convert(input)).isInstanceOf(IllegalArgumentException.class)
-                .hasMessageContaining("Decimal to roman converter does not support converting larger numbers than 3999 or negative numbers");
+            .hasMessageContaining("Decimal to roman converter does not support converting larger numbers than 3999 or negative numbers");
     }
 }
